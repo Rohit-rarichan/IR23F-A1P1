4e1f808ccaf26bb7bd069793edf7d8b22adca096
Rohit Rarichan <rraricha@uci.edu>
2024-01-22 13:25:52 -0800

    Adding the recursive and files function

--- a1.py
+++ a1.py
@@ -6,13 +6,29 @@ def direc_list(list_dir):  #list_dir contains ['L', '/home/algol/ics32/lectures'
         print(currentpath)
 
 
+def single_call_function(list_dir, myPath):
+    if list_dir[2] == '-r':
+        recursive_list(myPath)
+    elif list_dir[2] == '-f':
+        only_files(myPath)
+
+def recursive_list(myPath):
+    for currentpath in myPath.iterdir():
+        if currentpath.is_file():
+            print(currentpath)
+        elif currentpath.is_dir():
+            print(currentpath)
+            recursive_list(currentpath)
+
+def only_files(myPath):
+    for currentpath in myPath.iterdir():
+        if currentpath.is_file():
+            print(currentpath)
 
 def main(Command):
     list_dir = Command.split()
     if len(list_dir) == 2:
         direc_list(list_dir)
-
-
 
 if __name__ == "__main__":
     while True:

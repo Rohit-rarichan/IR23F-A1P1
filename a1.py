10087471b8e6728909b7f788def9295d11c60ef3
Rohit Rarichan <rraricha@uci.edu>
2024-01-22 22:28:31 -0800

    Fixing errors and adding the search files function

--- a1.py
+++ a1.py
@@ -1,16 +1,9 @@
 from pathlib import Path
 
-def direc_list(list_dir):  #list_dir contains ['L', '/home/algol/ics32/lectures']
-    myPath = Path(list_dir[1])
+def direc_list(myPath):  #list_dir contains ['L', '/home/algol/ics32/lectures']
     for currentpath in myPath.iterdir():
         print(currentpath)
 
-
-def single_call_function(list_dir, myPath):
-    if list_dir[2] == '-r':
-        recursive_list(myPath)
-    elif list_dir[2] == '-f':
-        only_files(myPath)
 
 def recursive_list(myPath):
     for currentpath in myPath.iterdir():
@@ -25,10 +18,29 @@ def only_files(myPath):
         if currentpath.is_file():
             print(currentpath)
 
+def file_search(myPath, filename):  #change the variable to filename
+    for currentpath in myPath.iterdir():
+        if currentpath.is_file() and currentpath.name.lower() == filename.lower():
+            print(currentpath)
+
 def main(Command):
     list_dir = Command.split()
+    myPath = Path(list_dir[1])
     if len(list_dir) == 2:
-        direc_list(list_dir)
+        direc_list(list_dir, myPath)
+    elif len(list_dir) == 3:
+        if list_dir[2] == '-r':
+            recursive_list(myPath)
+        elif list_dir[2] == '-f':
+            only_files(myPath)
+    elif len(list_dir) == 4:
+        if list_dir[2] == '-s':
+            filename = list_dir[3]
+            file_search(myPath, filename)
+        elif list_dir[2] == '-e':
+            ext = list_dir[3]
+            search_by_extension(myPath, ext)
+
 
 if __name__ == "__main__":
     while True:
